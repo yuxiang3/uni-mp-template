d0cd2b81d427ea64e68e934855aff4619239f14f
yuxiang <565671781@qq.com>
2023-11-15 20:04:10 +0800

    会员信息页

--- src/pagesMember/profile/profile.vue
+++ src/pagesMember/profile/profile.vue
@@ -1,15 +1,195 @@
 <script setup lang="ts">
-//
-</script>
-//修改样式
-<template>
-  <view class="index">index</view>
-  <!-- <div class="flex justify-center h-100 items-center">
-    <div class="w-10 h-10 bg-pink-500"></div>
-  </div>
-  <div class="test bg-yellow-200 h-100 w-730"></div> -->
-</template>
+ import { codeToText } from '@/utils/element-china-area-data.mjs'
+ // 获取屏幕边界到安全区域距离
+ const { safeAreaInsets } = uni.getSystemInfoSync()
+ // 完整地区
+const fullLocation = ref<string>('')
+ // 获取个⼈信息
+ const profile = ref<ProfileDetail>()
+ const getMemberProfileData = async () => {
+ const res = await getMemberProfileAPI()
+ profile.value = res.result
+ // codeToText[地区码] 匹配地区名称，但是 element-china-area-data ⾥的code去掉了末尾的0，所以去要使⽤正则表达式替换
+ fullLocation.value =
+ codeToText[profile.value.provinceCode?.replace(/0+$/, '')] +
+ ' ' +
+ codeToText[profile.value.cityCode?.replace(/0+$/, '')] +
+ ' ' +
+ codeToText[profile.value.countyCode?.replace(/0+$/, '')]
+}
+ onLoad(() => {
+ getMemberProfileData()
+ })
 
+</script>
+<template>
+ <view class="viewport">
+ <!-- 导航栏 -->
+ <view class="navbar" :style="{ paddingTop: safeAreaInsets?.top + 'px' }">
+ <navigator open-type="navigateBack" class="back icon-left" hover-cl
+ass="none"></navigator>
+ <view class="title">个⼈信息</view>
+ </view>
+ <!-- 头像 -->
+ <view class="avatar">
+ <view class="avatar-content">
+ <image class="image" src="profile?.avatar" mode="aspectFill" />
+ <text class="text">点击修改头像</text>
+ </view>
+ </view>
+ <!-- 表单 -->
+ <view class="form">
+ <!-- 表单内容 -->
+ <view class="form-content">
+ <view class="form-item">
+ <text class="label">账号</text>
+ <text class="account">{{ profile?.account }}</text>
+ </view>
+ <view class="form-item">
+ <text class="label">昵称</text>
+ <input class="input" type="text" placeholder="请填写昵称" value=
+ "profile?.nickname" />
+ </view>
+ <view class="form-item">
+ <text class="label">性别</text>
+ <radio-group>
+ <label class="radio">
+ <radio value="0" color="#27ba9b" :checked="profile?.gender=== 0" />
+ 男
+ </label>
+ <label class="radio">
+ <radio value="1" color="#27ba9b" :checked="profile?.gender=== 1" />
+ ⼥
+ </label>
+</radio-group>
+ </view>
+ <view class="form-item">
+ <text class="label">⽣⽇</text>
+ <picker class="picker" mode="date" start="1900-01-01" :end="new
+ Date()" :value="profile?.birthday">
+ <view v-if="profile?.birthday">{{ profile?.birthday }}</view>
+ <view class="placeholder" v-else>请选择⽇期</view>
+ </picker>
+ </view>
+ <view class="form-item">
+ <text class="label">城市</text>
+ <picker class="picker" mode="region" :value="fullLocation?.split
+(' ')">
+ <view v-if="profile?.fullLocation">{{ fullLocation }}</view>
+ <view class="placeholder" v-else>请选择城市</view>
+ </picker>
+ </view>
+ <view class="form-item">
+ <text class="label">职业</text>
+ <input class="input" type="text" placeholder="请填写职业" :value="profile?.profession" />
+ </view>
+ </view>
+ <!-- 提交按钮 -->
+ <button class="form-button">保 存</button>
+ </view>
+ </view>
+</template>
 <style lang="scss">
-//
-</style>
+page {
+ background-color: #f4f4f4;
+}
+.viewport {
+ display: flex;
+ flex-direction: column;
+ height: 100%;
+ background-image: url(https://pcapi-xiaotuxian-front-devtest.itheima.ne
+t/miniapp/images/order_bg.png);
+ background-size: auto 420rpx;
+ background-repeat: no-repeat;
+}
+// 导航栏
+.navbar {
+ position: relative;
+ .title {
+ height: 40px;
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ font-size: 16px;
+ font-weight: 500;
+ color: #fff;
+ }
+ .back {
+ position: absolute;
+ height: 40px;
+ width: 40px;
+ left: 0;
+ font-size: 20px;
+ color: #fff;
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ }
+}
+// 头像
+.avatar {
+ text-align: center;
+ width: 100%;
+ height: 260rpx;
+ display: flex;
+ flex-direction: column;
+ justify-content: center;
+ align-items: center;
+ .image {
+ width: 160rpx;
+ height: 160rpx;
+ border-radius: 50%;
+ background-color: #eee;
+ }
+ .text {
+ display: block;
+ padding-top: 20rpx;
+ line-height: 1;
+ font-size: 26rpx;
+ color: #fff;
+ }
+}
+// 表单
+.form {
+ background-color: #f4f4f4;
+ &-content {
+ margin: 20rpx 20rpx 0;
+ padding: 0 20rpx;
+ border-radius: 10rpx;
+ background-color: #fff;
+ }
+ &-item {
+ display: flex;
+ height: 96rpx;
+ line-height: 46rpx;
+ padding: 25rpx 10rpx;
+ background-color: #fff;
+ font-size: 28rpx;
+ border-bottom: 1rpx solid #ddd;
+ &:last-child {
+ border: none;
+ }
+ .label {
+ width: 180rpx;
+ color: #333;
+ }
+ .account {
+ color: #666;
+ }
+ .input {
+ flex: 1;
+ display: block;
+ height: 46rpx;
+ }
+ .radio {
+ margin-right: 20rpx;
+ }
+ .picker {
+ flex: 1;
+ }
+ .placeholder {
+ color: #808080;
+ }
+ }
+ &-button {
+ height: 80rpx;}}
--- src/types/params.d.ts
+++ src/types/params.d.ts
@@ -17,3 +17,25 @@ declare type LoginParams = {
   // encryptedData?: string
   // iv?: string
 }
+/** 个⼈信息 ⽤户详情信息 */
+declare type ProfileDetail = {
+  /** ⽤户ID */
+  id: number
+  /** 头像 */
+  avatar: string
+  /** 账户名 */
+  account: string
+  /** 昵称 */
+  nickname?: string
+  /** 性别 */
+  gender?: Gender
+  /** ⽣⽇ */
+  birthday?: string
+  /** 省市区 */
+  fullLocation?: string
+  /** 职业 */
+  profession?: string
+}
+/** 性别 */
+declare type Gender = '⼥' | '男'
+/** 个⼈信息 修改请求体参数 */
